5f121751429fe420e8f48ea56bf9cf6ce689eef8
YeonwooSung <neos960518@gmail.com>
2022-09-30 22:57:51 +0900

    Change response type from json to streaming res

--- tigas/api/v1/generate.py
+++ tigas/api/v1/generate.py
@@ -1,7 +1,7 @@
 import base64
 from fastapi import APIRouter
-from fastapi.responses import JSONResponse
-import io
+from fastapi.responses import FileResponse, StreamingResponse, JSONResponse
+from io import BytesIO
 import torch
 from PIL import Image
 
@@ -20,7 +20,7 @@ def from_image_to_bytes(img):
     pillow image 객체를 bytes로 변환
     """
     # Pillow 이미지 객체를 Bytes로 변환
-    imgByteArr = io.BytesIO()
+    imgByteArr = BytesIO()
     img.save(imgByteArr, format=img.format)
     imgByteArr = imgByteArr.getvalue()
     # Base64로 Bytes를 인코딩
@@ -42,8 +42,9 @@ async def get_user():
     sample_text = 'Dogs running on a beach'
     sample_image = model(sample_text)
     pil_image = convert_model_generate_img_to_pillow_img(sample_image)
-    img_converted = from_image_to_bytes(pil_image)
-    return JSONResponse([img_converted])
+    return StreamingResponse(BytesIO(pil_image), media_type="image/png")
+    # img_converted = from_image_to_bytes(pil_image)
+    # return JSONResponse([img_converted])
 
 
 @router.post("/", tags=["generate"])
