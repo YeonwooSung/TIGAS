f953544b530c4cb374c125af1a942427fe86c2ad
YeonwooSung <neos960518@gmail.com>
2022-10-12 05:13:43 +0000

    Test commit

--- tigas/app.py
+++ tigas/app.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
     try:
         server.add_endpoint('/signin', 'signin', signin_router)
     except:
-        pass
+        print('failed to add endpoints')
     uvicorn.run(server.app, host="0.0.0.0", port=5000)
