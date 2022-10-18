6822dd5fa51257feef4c05d30d23a58de8e70766
YeonwooSung <neos960518@gmail.com>
2022-10-18 11:13:40 +0900

    Remove unused endpoints

--- tigas/api/v1/__init__.py
+++ tigas/api/v1/__init__.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
-from .signin import router as signin_router
 from .generate import router as generate_router
 
 v1 = FastAPI()
-v1.include_router(signin_router, prefix="/signin", tags=["signin"])
 v1.include_router(generate_router, prefix="/generate", tags=["generate"])
 
 @v1.get("/")
--- tigas/api/v1/signin.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from fastapi.responses import RedirectResponse
-
-
-router = APIRouter()
-
-@router.get("/", tags=["signin"])
-async def get_user():
-    return RedirectResponse(url="/signin")
