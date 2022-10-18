d85a52a7afcaf2a8ade01dde555dfcb75c10fb61
YeonwooSung <neos960518@gmail.com>
2022-10-18 14:26:35 +0900

    Update pipeline to support img2img

--- tigas/api/utils/model.py
+++ tigas/api/utils/model.py
@@ -2,8 +2,27 @@ import torch
 from torch import nn, autocast
 from transformers import CLIPTextModel, CLIPTokenizer
 from diffusers import AutoencoderKL, UNet2DConditionModel, LMSDiscreteScheduler
-
+import numpy as np
+from PIL import Image
 import yaml
+
+
+def preprocess(image):
+    '''
+    Preprocesses the image to be fed into the model.
+
+    1) resize to integer multiple of 32
+    2) use Lanczos algorithm to sampling the most useful pixels
+    3) reshape the image
+    4) convert to tensor
+    '''
+    w, h = image.size
+    w, h = map(lambda x: x - x % 32, (w, h))  # resize to integer multiple of 32
+    image = image.resize((w, h), resample=Image.LANCZOS)
+    image = np.array(image).astype(np.float32) / 255.0
+    image = image[None].transpose(0, 3, 1, 2)
+    image = torch.from_numpy(image)
+    return 2.0 * image - 1.0
 
 
 with open('tigas.yaml') as f:
@@ -41,6 +60,10 @@ class ModelConfig:
 
 
 class CustomTextToImageModel(nn.Module):
+    '''
+    Custom pipeline model for image generation.
+    Based on the Stable Diffusion pipeline.
+    '''
     def __init__(self, config, device, from_pretrained=False):
         super().__init__()
         self.config = config
@@ -49,6 +72,7 @@ class CustomTextToImageModel(nn.Module):
 
         # Seed generator to create the inital latent noise
         self.generator=torch.manual_seed(32)
+        self.generator_img2img = torch.Generator(device=device).manual_seed(1004)
         
         # check whether to load model from pretrained weights or saved weights file
         if from_pretrained:
@@ -69,7 +93,33 @@ class CustomTextToImageModel(nn.Module):
         self.uncond_input = [""] * config.batch_size
 
 
-    def forward(self, prompt):
+    def forward(self, prompt:str, use_text_inversion:bool=True, img:Image=None, img_path:str=None):
+        if use_text_inversion:
+            return self.forward_text_inversion(prompt)
+        else:
+            if img:
+                return self.forward_prompt_guided_style_transfer(prompt, img)
+            elif img_path:
+                # use "convert("RGB")" to convert to RGB if image is not RGB such as RGBA or CMYK
+                img = Image.open(img_path).convert("RGB")
+                # resize image to match model input size
+                img = img.resize((512, 512))
+                return self.forward_prompt_guided_style_transfer(prompt, img)
+            else:
+                raise ValueError("Please provide either an image or an image path")
+
+
+    def forward_prompt_guided_style_transfer(self, prompt:str, img:Image):
+        latents = self.setup_scheduler_img2img(img)
+        latents = latents.to(self.device)
+        text_embeddings = self.embed_prompts(prompt)
+        latents = self.generate_latent_vector_with_unet_img2img(text_embeddings, latents)
+        latents = self.latent_scaling_factor * latents
+        image = self.kl_model.decode(latents).sample
+        return image
+
+
+    def forward_text_inversion(self, prompt):
         latents = self.setup_scheduler()
         latents = latents.to(self.device)
         text_embeddings = self.embed_prompts(prompt)
@@ -101,7 +151,15 @@ class CustomTextToImageModel(nn.Module):
                 return self.run_denoising_loop(text_embeddings, latents)
         else:
             return self.run_denoising_loop(text_embeddings, latents)
-    
+
+    def generate_latent_vector_with_unet_img2img(self, text_embeddings, latents):
+        if self.device == 'cuda':
+            with autocast('cuda'):
+                return self.run_denoising_loop_img2img(text_embeddings, latents)
+        else:
+            return self.run_denoising_loop_img2img(text_embeddings, latents)
+
+
     def run_denoising_loop(self, text_embeddings, latents):
         scheduler = self.scheduler
         for i, t in enumerate(scheduler.timesteps):
@@ -121,19 +179,98 @@ class CustomTextToImageModel(nn.Module):
             latents = scheduler.step(noise_pred, i, latents).prev_sample
         return latents
 
+    
+    def run_denoising_loop_img2img(self, text_embeddings, init_latents):
+        num_inference_steps = self.num_inference_steps
+        strength = 0.8#TODO
+        batch_size, num_images_per_prompt = 1, 1
+
+        # initialize the timesteps
+        init_timestep = int(num_inference_steps * strength)
+        init_timestep = min(init_timestep, num_inference_steps)
+        timesteps = self.scheduler.timesteps[-init_timestep]
+
+        print('dtype of timesteps(1):', timesteps.dtype)
+        timesteps = torch.tensor([timesteps] * batch_size * num_images_per_prompt, device=self.device)
+        print('dtype of timesteps(2):', timesteps.dtype)
+        latents_dtype = text_embeddings.dtype
+
+        noise = torch.randn(init_latents.shape, generator=self.generator_img2img, device=self.device, dtype=latents_dtype)
+        init_latents = self.scheduler.add_noise(init_latents, noise, timesteps)
+        latents = init_latents
+
+        t_start = max(num_inference_steps - init_timestep, 0)
+
+        # Some schedulers like PNDM have timesteps as arrays
+        # It's more optimized to move all timesteps to correct device beforehand
+        timesteps = self.scheduler.timesteps[t_start:].to(self.device)
+
+        # for i, t in enumerate(self.scheduler.timesteps):
+        for i, t in enumerate(timesteps):
+            # print(latents.shape)
+            # expand the latents if we are doing classifier free guidance
+            latent_model_input = torch.cat([latents] * 2)
+            
+            latent_model_input = self.scheduler.scale_model_input(latent_model_input, t)
+
+            # predict the noise residual
+            noise_pred = self.unet_model(latent_model_input, t, encoder_hidden_states=text_embeddings).sample
+            
+            # perform guidance
+            noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+            noise_pred = noise_pred_uncond + self.config.guidance_scale * (noise_pred_text - noise_pred_uncond)
+
+            # compute the previous noisy sample x_t -> x_t-1
+            latents = self.scheduler.step(noise_pred, t, latents).prev_sample
+        return latents
 
 
-    def setup_scheduler(self):
+    def setup_scheduler(self) -> torch.Tensor:
+        '''
+        Setup the diffusion scheduler for text inversion
+
+        In this project, the LMSDiscreteScheduler is used, where the PNDMScheduler is a default one for the StableDiffusion pipeline.
+        The main reason that we choosed to use LMSDiscreteScheduler for diffusion model is that the quality of the generated images is better.
+        '''
         latents = torch.randn(
             (self.config.batch_size, self.unet_model.in_channels, self.config.height // 8, self.config.width // 8),
             generator=self.generator,
         )
-        num_inference_steps = self.config.num_inference_steps
-        self.scheduler.set_timesteps(num_inference_steps)
+        self.num_inference_steps = self.config.num_inference_steps
+        self.scheduler.set_timesteps(self.num_inference_steps)
         return latents * self.scheduler.sigmas[0]
 
 
-def run_model_test():
+    def setup_scheduler_img2img(self, img:Image) -> torch.Tensor:
+        '''
+        Setup the diffusion scheduler for image to image task.
+        
+        In this project, the LMSDiscreteScheduler is used, where the PNDMScheduler is a default one for the StableDiffusion pipeline.
+        The main reason that we choosed to use LMSDiscreteScheduler for diffusion model is that the quality of the generated images is better.
+
+        @param img: PIL image (prompt image)
+        '''
+        self.num_inference_steps = self.config.num_inference_steps
+        self.scheduler.set_timesteps(self.num_inference_steps)
+        
+        # preprocess the given image (resize, resample, and convert to tensor)
+        img = preprocess(img)
+        img = img.to(device=self.device)
+        
+        # encode the image with the VAE
+        latent_dist = self.kl_model.encode(img).latent_dist
+
+        # sample from the latent distribution
+        init_latents = latent_dist.sample(generator=self.generator_img2img)
+        init_latents = LATENT_SCALING_FACTOR * init_latents
+        return init_latents
+
+
+
+# --------------------- #
+# Codes for testing
+
+def run_model_test_for_text_inversion():
     config = ModelConfig()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = CustomTextToImageModel(config, device, from_pretrained=False)
@@ -148,5 +285,6 @@ def run_model_test():
     pil_images = [Image.fromarray(image) for image in images]
     pil_images[0].save('sample.png')
 
+
 if __name__ == '__main__':
-    run_model_test()
+    run_model_test_for_text_inversion()
