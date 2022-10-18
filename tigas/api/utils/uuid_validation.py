63a829bae9e376c2b57850d52d762aad687b5a73
YeonwooSung <neos960518@gmail.com>
2022-10-18 17:02:43 +0900

    Validate uuid with a custom multi-level validation

--- tigas/api/utils/uuid_validation.py
+++ tigas/api/utils/uuid_validation.py
@@ -1,5 +1,18 @@
+UUID_LEN = 36
 
+
+def validate_uuid_encoding(uuid_str:str):
+    try:
+        uuid_str.encode('ascii')
+    except UnicodeEncodeError:
+        return False
+    return True
+
+def validate_uuid_len(uuid_str:str):
+    return len(uuid_str) == UUID_LEN
 
 def validate_uuid(uuid_str:str):
-    #TODO
-    return True
+    # Multi-levl validation
+    #   1) check if encoding type is ascii
+    #   2) check if length is 36
+    return validate_uuid_encoding(uuid_str) and validate_uuid_len(uuid_str)
