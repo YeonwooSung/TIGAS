e70346d63960231bff66066a4531f546e2cb1eb1
YeonwooSung <neos960518@gmail.com>
2022-10-18 17:05:48 +0900

    Add type checking for return values

--- tigas/api/utils/uuid_validation.py
+++ tigas/api/utils/uuid_validation.py
@@ -1,17 +1,17 @@
 UUID_LEN = 36
 
 
-def validate_uuid_encoding(uuid_str:str):
+def validate_uuid_encoding(uuid_str:str) -> bool:
     try:
         uuid_str.encode('ascii')
     except UnicodeEncodeError:
         return False
     return True
 
-def validate_uuid_len(uuid_str:str):
+def validate_uuid_len(uuid_str:str) -> bool:
     return len(uuid_str) == UUID_LEN
 
-def validate_uuid(uuid_str:str):
+def validate_uuid(uuid_str:str) -> bool:
     # Multi-levl validation
     #   1) check if encoding type is ascii
     #   2) check if length is 36
