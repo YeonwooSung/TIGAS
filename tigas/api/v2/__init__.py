4bd2b9ce90a99593f75cdf29c16744c658e9ae3b
YeonwooSung <neos960518@gmail.com>
2022-10-18 16:12:55 +0900

    Add /api/v2/i2i for image2image API

--- tigas/api/v2/__init__.py
+++ tigas/api/v2/__init__.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
+# custom module
+from .generate import router as generate_router
+
 v2 = FastAPI()
-# v2.include_router(generate_router, prefix="/generate", tags=["generate"])
+v2.include_router(generate_router, prefix="/generate", tags=["generate"])
 
 @v2.get("/")
 async def root():
--- tigas/api/v2/generate.py
+++ tigas/api/v2/generate.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request, HTTPException, Form
+from fastapi import APIRouter, Request, HTTPException, Form, UploadFile, File
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import FileResponse, JSONResponse
 import os
@@ -43,23 +43,24 @@ i2i_logger = utils.StableLogger(f'{LOG_DIR_PATH}i2i.log', name='i2i_logger')
 router = APIRouter()
 
 
-@router.post('/i2i', tags=['generate', 'i2i'])
-async def i2i(request: Request, text: str = Form(...), image: bytes = Form(...)):
+@router.post('/i2i', tags=['generate'])
+async def i2i(request: Request, text: str = Form(...), image: UploadFile = Form(...)):
     """
     text와 image를 multipart/form-data로 받아서 이미지를 생성하는 API
     """
-    #TODO ??
     # check if the queue is full
     if get_queue_len() >= MAX_LEN:
         raise HTTPException(status_code=429, detail='Queue is full')
 
+
     # generate uuid
     uuid_str = str(uuid.uuid4())
 
+    contents = await image.read()
     # save image
-    img_path = f'{IMG_DIR_PATH}{uuid_str}.jpg'
+    img_path = f'{IMG_DIR_PATH}{uuid_str}_u.png'
     with open(img_path, 'wb') as f:
-        f.write(image)
+        f.write(contents)
 
     # append to queue
     obj = utils.TIGAS_Form(prompt=text, uuid=uuid_str, type='i2i')
@@ -67,3 +68,57 @@ async def i2i(request: Request, text: str = Form(...), image: bytes = Form(...))
 
     # return uuid
     return {'uuid': uuid_str}
+
+
+@router.get("/i2i/queue", tags=["generate"])
+async def get_size_of_waiting_queue():
+    return JSONResponse(content={"num_of_waiting": get_queue_len()})
+
+@router.get("/i2i/queue/{uuid}", tags=["generate"])
+async def get_info_of_waiting_queue(uuid: str):
+    index = get_object_index_by_uuid()
+    if index != -1:
+        return JSONResponse(content={"uuid": uuid, "index": index})
+    return HTTPException(status_code=400, detail="Not in the waiting list")
+
+
+@router.get("/i2i/{uuid}/img", tags=["generate"])
+async def get_image_from_uuid(uuid: str):
+    try:
+        # check if uuid is valid
+        # if not utils.is_valid_uuid(uuid):
+        #     i2i_logger.log(f'/i2i/{uuid} :: error="Invalid UUID"', level='warning')
+        #     return HTTPException(status_code=400, detail="Invalid UUID")
+        
+        # check if image exists
+        if not os.path.isfile(f'{IMG_DIR_PATH}{uuid}.png'):
+            i2i_logger.log(f'/i2i/{uuid} :: error="Image not found"', level='warning')
+            return HTTPException(status_code=404, detail="Image not found")
+        
+        # return image
+        i2i_logger.log(f'/i2i/{uuid} :: success')
+        return FileResponse(f'{IMG_DIR_PATH}{uuid}.png')
+    except Exception as e:
+        i2i_logger.log(f'/i2i/{uuid} :: error="{e}"', level='error')
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+
+
+@router.get("/i2i/{uuid}/status", tags=["generate"])
+async def get_status_from_uuid(uuid: str):
+    try:
+        # check if uuid is valid
+        # if not utils.is_valid_uuid(uuid):
+        #     i2i_logger.log(f'/i2i/{uuid}/status :: error="Invalid UUID"', level='warning')
+        #     return HTTPException(status_code=400, detail="Invalid UUID")
+        
+        # check if image exists
+        if not os.path.isfile(f'{IMG_DIR_PATH}{uuid}.png'):
+            i2i_logger.log(f'/i2i/{uuid}/status :: error="Image not found"', level='warning')
+            return HTTPException(status_code=404, detail="Image not found")
+        
+        # return image
+        i2i_logger.log(f'/i2i/{uuid}/status :: exists')
+        return JSONResponse(content={"status": "ok"})
+    except Exception as e:
+        i2i_logger.log(f'/i2i/{uuid}/status :: error="{e}"', level='error')
+        raise HTTPException(status_code=500, detail="Internal Server Error")
