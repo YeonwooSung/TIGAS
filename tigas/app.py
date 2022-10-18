4c425a6b77645ba6c57130e62bbb3e8559513250
YeonwooSung <neos960518@gmail.com>
2022-10-18 16:10:17 +0900

    Add new attributes to the config

--- tigas/app.py
+++ tigas/app.py
@@ -14,12 +14,14 @@ def parse_config():
         port = int(tigas_conf['port'] if 'port' in tigas_conf else '5000')
         host = tigas_conf['host'] if 'host' in tigas_conf else '0.0.0.0'
         name = tigas_conf['name'] if 'name' in tigas_conf else 'tigas'
-        return port, host, name
+        reload = tigas_conf['reload'] if 'reload' in tigas_conf else False
+        debug = tigas_conf['debug'] if 'debug' in tigas_conf else False
+        return port, host, name, reload, debug
 
 
 
 if __name__ == '__main__':
-    port, host, name = parse_config()
+    port, host, name, reload, debug = parse_config()
     server = Server(name=name)
     try:
         server.add_endpoint('/signin', 'signin', signin_router)
@@ -30,4 +32,10 @@ if __name__ == '__main__':
     inference_thread = threading.Thread(target=inference_loop)
     inference_thread.start()
 
-    uvicorn.run(server.app, host=host, port=port)
+    uvicorn.run(
+        server.app, 
+        host=host, 
+        port=port, 
+        reload=reload, 
+        debug=debug,
+    )
